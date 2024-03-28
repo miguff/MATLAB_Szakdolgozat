844a49121ff15418f49993aa59c660e151157c54
Miguff <mihgeza2000@gmail.com>
2024-03-29 00:30:09 +0100

    2D done
    Úgy néz ki a 2D-s modellezzésben már csak az A* van vissza, hogy mindegyik térképre megcsináljam

--- exampleHelperDerivative.m
+++ exampleHelperDerivative.m
@@ -1,6 +1,8 @@
 function sdot2 = exampleHelperDerivative(t,s,model,desiredPosition,desiredYaw,kp,kd,ki,kyaw)
 % Copyright 2022 The MathWorks, Inc.
 
+s
+
 % global prevError prevTime
 
 DroneMass = model.Configuration.Mass;
